bfb7049df4d5b0fc877f0a540c5440cf550f98e2
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-05-21 10:16:33 +0200

    ⠀⠀⠀⠀⠀

--- Editor/DateDrawer.cs
+++ Editor/DateDrawer.cs
@@ -10,16 +10,16 @@ namespace UnityEssentials
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            if (property.propertyType != SerializedPropertyType.Vector3)
+            if (property.propertyType != SerializedPropertyType.Vector3Int)
             {
-                EditorGUI.HelpBox(position, "DateAttribute attribute only supports Vector3 fields.", MessageType.Error);
+                EditorGUI.HelpBox(position, "DateAttribute attribute only supports Vector3Int fields.", MessageType.Error);
                 return;
             }
 
             EditorGUI.BeginProperty(position, label, property);
 
-            // Get current values from Vector3
-            var vectorValue = property.vector3Value;
+            // Get current values from Vector3Int
+            var vectorValue = property.vector3IntValue;
             var dateContainer = new DateContainer
             {
                 Day = (Day)(vectorValue.x == 0 ? 1 : vectorValue.x),
@@ -31,64 +31,51 @@ namespace UnityEssentials
             int indentLevel = EditorGUI.indentLevel;
             EditorGUI.indentLevel = 0;
 
-            float spacing = 16;
             float labelWidth = EditorGUIUtility.labelWidth;
-            float valueAreaWidth = position.width - EditorGUIUtility.labelWidth - spacing * 3 - 3 * 3 - 1;
+            float valueAreaWidth = position.width - EditorGUIUtility.labelWidth - 1;
             float fieldWidth = valueAreaWidth / 3f;
             float lineHeight = EditorGUIUtility.singleLineHeight;
-            float fieldXPosition = 20 + labelWidth;
+            float fieldXPosition = 18 + labelWidth;
 
-            // Day field (D)
-            EditorGUI.LabelField(new Rect(fieldXPosition, position.y, fieldWidth, lineHeight), "D");
-            var dayPosition = new Rect(fieldXPosition + spacing, position.y, fieldWidth, lineHeight);
-            //dateContainer.Day = (Day)EditorGUI.EnumPopup(dayPosition, dateContainer.Day);
-            fieldXPosition += fieldWidth + 20;
+            var dayPosition = new Rect(fieldXPosition, position.y, fieldWidth, lineHeight);
+            var monthPosition = new Rect(dayPosition.x + fieldWidth, position.y, fieldWidth, lineHeight);
+            var yearPositon = new Rect(monthPosition.x + fieldWidth, position.y, fieldWidth, lineHeight);
 
-            //var enumNames = Enum.GetNames(typeof(Day));
-            //if (EditorGUI.DropdownButton(dayPosition, new GUIContent(dateContainer.Day.ToString()), FocusType.Keyboard))
-            //    EnumSearchPopup.Show(dayPosition, property.serializedObject.targetObject, property.propertyPath, typeof(Day), dateContainer.Day, enumNames);
-
-            // Month field (M)
-
-            EditorGUI.LabelField(new Rect(fieldXPosition, position.y, fieldWidth, lineHeight), "M");
-            var monthPosition = new Rect(fieldXPosition + spacing, position.y, fieldWidth, lineHeight);
-            dateContainer.Month = (Month)EditorGUI.EnumPopup(monthPosition, dateContainer.Month);
-            fieldXPosition += fieldWidth + 20;
-
-            // Year field (Y)
-            EditorGUI.LabelField(new Rect(fieldXPosition, position.y, fieldWidth, lineHeight), "Y");
-            var yearPosition = new Rect(fieldXPosition + spacing, position.y, fieldWidth, lineHeight);
-            dateContainer.Year = (Year)EditorGUI.EnumPopup(yearPosition, dateContainer.Year);
-
-            // Validate day based on month and year
-            int yearInt = GetYearFromEnum(dateContainer.Year);
-            int maxDay = GetMaxDay(dateContainer.Month, yearInt);
-            int currentDay = (int)dateContainer.Day;
-
-            if (currentDay > maxDay)
-                dateContainer.Day = (Day)maxDay;
-
-            property.vector3Value = new Vector3(
-                (float)dateContainer.Day,
-                (float)dateContainer.Month,
-                (float)dateContainer.Year);
+            EnumDrawer.EnumPopup<Day>(dayPosition, dateContainer.Day, (newDay) => UpdateProperty(property, dateContainer.UpdateDay(newDay)));
+            EnumDrawer.EnumPopup<Month>(monthPosition, dateContainer.Month, (newMonth) => UpdateProperty(property, dateContainer.UpdateMonth(newMonth)));
+            EnumDrawer.EnumPopup<Year>(yearPositon, dateContainer.Year, (newYear) => UpdateProperty(property, dateContainer.UpdateYear(newYear)));
 
             EditorGUI.indentLevel = indentLevel;
             EditorGUI.EndProperty();
         }
 
-        private int GetYearFromEnum(Year yearEnum)
+        private void UpdateProperty(SerializedProperty property, DateContainer dateContainer)
         {
-            string yearString = yearEnum.ToString().Substring(1);
+            int yearInt = GetYearFromEnum(dateContainer.Year);
+            int maxDay = GetMaxDay(dateContainer.Month, yearInt);
+            if ((int)dateContainer.Day > maxDay)
+                dateContainer.Day = (Day)maxDay;
+
+            property.vector3IntValue = new Vector3Int(
+                (int)dateContainer.Day,
+                (int)dateContainer.Month,
+                (int)dateContainer.Year);
+
+            Debug.Log(dateContainer);
+        }
+
+        private int GetYearFromEnum(Year YearEnum)
+        {
+            string yearString = YearEnum.ToString().Substring(1);
             return int.Parse(yearString);
         }
 
         private int GetMaxDay(Month month, int year) =>
             month switch
             {
-                Month.Jan or Month.Mar or Month.May or Month.Jul or Month.Aug or Month.Oct or Month.Dec => 31,
-                Month.Apr or Month.Jun or Month.Sep or Month.Nov => 30,
-                Month.Feb => IsLeapYear(year) ? 29 : 28,
+                Month.January or Month.March or Month.May or Month.July or Month.August or Month.October or Month.December => 31,
+                Month.April or Month.June or Month.September or Month.November => 30,
+                Month.Febuary => IsLeapYear(year) ? 29 : 28,
                 _ => 31,
             };
 
@@ -102,39 +89,62 @@ namespace UnityEssentials
         }
     }
 
-    [System.Serializable]
+    [Serializable]
     public struct DateContainer
     {
         public Month Month;
         public Day Day;
         public Year Year;
+
+        public DateContainer UpdateDay(Day newDay)
+        {
+            Day = newDay;
+            return this;
+        }
+
+        public DateContainer UpdateMonth(Month newMonth)
+        {
+            Month = newMonth;
+            return this;
+        }
+
+        public DateContainer UpdateYear(Year newYear)
+        {
+            Year = newYear;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return $"{Day} {Month} {Year}";
+        }
     }
 
-    public enum Month { Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec }
-    public enum Day { D1 = 1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15, D16, D17, D18, D19, D20, D21, D22, D23, D24, D25, D26, D27, D28, D29, D30, D31 }
+    public enum Month { January = 1, Febuary, March, April, May, June, July, August, September, October, November, December }
+    public enum Day { _1 = 1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31 }
     public enum Year
     {
-        Y2100, Y2099, Y2098, Y2097, Y2096, Y2095, Y2094, Y2093, Y2092, Y2091,
-        Y2090, Y2089, Y2088, Y2087, Y2086, Y2085, Y2084, Y2083, Y2082, Y2081,
-        Y2080, Y2079, Y2078, Y2077, Y2076, Y2075, Y2074, Y2073, Y2072, Y2071,
-        Y2070, Y2069, Y2068, Y2067, Y2066, Y2065, Y2064, Y2063, Y2062, Y2061,
-        Y2060, Y2059, Y2058, Y2057, Y2056, Y2055, Y2054, Y2053, Y2052, Y2051,
-        Y2050, Y2049, Y2048, Y2047, Y2046, Y2045, Y2044, Y2043, Y2042, Y2041,
-        Y2040, Y2039, Y2038, Y2037, Y2036, Y2035, Y2034, Y2033, Y2032, Y2031,
-        Y2030, Y2029, Y2028, Y2027, Y2026, Y2025, Y2024, Y2023, Y2022, Y2021,
-        Y2020, Y2019, Y2018, Y2017, Y2016, Y2015, Y2014, Y2013, Y2012, Y2011,
-        Y2010, Y2009, Y2008, Y2007, Y2006, Y2005, Y2004, Y2003, Y2002, Y2001,
-        Y2000, Y1999, Y1998, Y1997, Y1996, Y1995, Y1994, Y1993, Y1992, Y1991,
-        Y1990, Y1989, Y1988, Y1987, Y1986, Y1985, Y1984, Y1983, Y1982, Y1981,
-        Y1980, Y1979, Y1978, Y1977, Y1976, Y1975, Y1974, Y1973, Y1972, Y1971,
-        Y1970, Y1969, Y1968, Y1967, Y1966, Y1965, Y1964, Y1963, Y1962, Y1961,
-        Y1960, Y1959, Y1958, Y1957, Y1956, Y1955, Y1954, Y1953, Y1952, Y1951,
-        Y1950, Y1949, Y1948, Y1947, Y1946, Y1945, Y1944, Y1943, Y1942, Y1941,
-        Y1940, Y1939, Y1938, Y1937, Y1936, Y1935, Y1934, Y1933, Y1932, Y1931,
-        Y1930, Y1929, Y1928, Y1927, Y1926, Y1925, Y1924, Y1923, Y1922, Y1921,
-        Y1920, Y1919, Y1918, Y1917, Y1916, Y1915, Y1914, Y1913, Y1912, Y1911,
-        Y1910, Y1909, Y1908, Y1907, Y1906, Y1905, Y1904, Y1903, Y1902, Y1901,
-        Y1900
+        _2100, _2099, _2098, _2097, _2096, _2095, _2094, _2093, _2092, _2091,
+        _2090, _2089, _2088, _2087, _2086, _2085, _2084, _2083, _2082, _2081,
+        _2080, _2079, _2078, _2077, _2076, _2075, _2074, _2073, _2072, _2071,
+        _2070, _2069, _2068, _2067, _2066, _2065, _2064, _2063, _2062, _2061,
+        _2060, _2059, _2058, _2057, _2056, _2055, _2054, _2053, _2052, _2051,
+        _2050, _2049, _2048, _2047, _2046, _2045, _2044, _2043, _2042, _2041,
+        _2040, _2039, _2038, _2037, _2036, _2035, _2034, _2033, _2032, _2031,
+        _2030, _2029, _2028, _2027, _2026, _2025, _2024, _2023, _2022, _2021,
+        _2020, _2019, _2018, _2017, _2016, _2015, _2014, _2013, _2012, _2011,
+        _2010, _2009, _2008, _2007, _2006, _2005, _2004, _2003, _2002, _2001,
+        _2000, _1999, _1998, _1997, _1996, _1995, _1994, _1993, _1992, _1991,
+        _1990, _1989, _1988, _1987, _1986, _1985, _1984, _1983, _1982, _1981,
+        _1980, _1979, _1978, _1977, _1976, _1975, _1974, _1973, _1972, _1971,
+        _1970, _1969, _1968, _1967, _1966, _1965, _1964, _1963, _1962, _1961,
+        _1960, _1959, _1958, _1957, _1956, _1955, _1954, _1953, _1952, _1951,
+        _1950, _1949, _1948, _1947, _1946, _1945, _1944, _1943, _1942, _1941,
+        _1940, _1939, _1938, _1937, _1936, _1935, _1934, _1933, _1932, _1931,
+        _1930, _1929, _1928, _1927, _1926, _1925, _1924, _1923, _1922, _1921,
+        _1920, _1919, _1918, _1917, _1916, _1915, _1914, _1913, _1912, _1911,
+        _1910, _1909, _1908, _1907, _1906, _1905, _1904, _1903, _1902, _1901,
+        _1900
     }
 }
 #endif
